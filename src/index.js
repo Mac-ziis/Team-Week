d5ed3f83b502a5a57a075fcec1087bf282094af8
Joseph Murray <josephwmurray@gmail.com>
2023-08-15 21:27:32 -0700

    integrate shopping option to main html page, apply stylings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import file name and path for other js files
+import Plant from './js/seed-catalog.js';
 
-// Business Logic
+// UI logic for seed-catalog
 
+document.addEventListener("DOMContentLoaded", function () {
+  
+  function calculateTax(cost) {
+    const taxRate = 0.18;
+    return cost * taxRate;
+  }
 
+  function updateCost() {
+    const selectedTypes = Array.from(document.querySelectorAll('input[name="plant-type"]:checked'));
+    let totalCost = 0;
 
-// UI Logic
+    selectedTypes.forEach(typeInput => {
+      const type = typeInput.value;
+      const quantityInput = document.querySelector(`input[name="quantity-${type}"]`);
+      const quantity = parseFloat(quantityInput.value);
 
+      if (quantity > 0) {
+        const plant = new Plant(type, quantity);
+        totalCost += plant.calculateCost();
+      }
+    });
+
+    const costDisplay = document.getElementById('total-cost');
+    costDisplay.textContent = "Total Cost: $" + totalCost.toFixed(2);
+
+    return totalCost;
+  }
+
+  function updateReceipt(totalCost) {
+    const totalCostDisplay = document.getElementById('total-cost');
+    const taxAmountDisplay = document.getElementById('tax-amount');
+    const totalWithTaxDisplay = document.getElementById('total-with-tax');
+
+    const tax = calculateTax(totalCost);
+    const totalWithTax = totalCost + tax;
+
+    totalCostDisplay.innerHTML = `Total Cost: $${totalCost.toFixed(2)}`;
+    taxAmountDisplay.innerHTML = `Tax: $${tax.toFixed(2)}`;
+    totalWithTaxDisplay.innerHTML = `Total with Tax: $${totalWithTax.toFixed(2)}`;
+  }
+
+  function showReceipt() {
+    const receipt = document.getElementById('receipt');
+    receipt.classList.remove('hidden');
+  }
+
+  const placeOrderButton = document.getElementById('place-order-btn');
+  placeOrderButton.addEventListener('click', () => {
+    const totalCost = updateCost();
+    updateReceipt(totalCost);
+    showReceipt();
+  });
+
+  const shoppingButton = document.getElementById('shopping-btn');
+  shoppingButton.addEventListener('click', () => {
+    //make shit hidden
+  });
+});
