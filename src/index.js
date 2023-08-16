92cf0a3982e57f7a902cae75240fe767a88728a8
Mac-ziis <macg286@gmail.com>
2023-08-15 19:55:13 -0700

    Add checkboxes and quantity options to the order form

--- src/index.js
+++ src/index.js
@@ -3,106 +3,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Plant from './js/seed-catalog.js';
 
-// Business Logic
-
-
-
-// UI Logic for seed-catalog.js
-// document.addEventListener('DOMContentLoaded', function () {
-
-// function calculateTax(cost) {
-//   const taxRate = 0.18;
-//   return cost * taxRate;
-// }
-
-// function updateCost() {
-//   const selectedType = document.querySelector('select[name="plant-type"]').value;
-//   const quantity = parseFloat(document.getElementById('quantity').value);
-//   const plant = new Plant(selectedType, quantity);
-//   const cost = plant.calculateCost();
-//   const costDisplay = document.getElementById('cost');
-//   costDisplay.textContent = "Cost: $" + cost.toFixed(2);
-
-//   return cost;
-// }
-
-// function updateReceipt(plant) {
-//   const orderDetails = document.getElementById('order-details');
-//   orderDetails.innerHTML = `Type: ${plant.type}<br>Quantity: ${plant.quantity}`;
-
-//   const cost = plant.calculateCost();
-//   const tax = calculateTax(cost);
-//   const total = cost + tax;
-
-//   const totalCost = document.getElementById('total-cost');
-//   const taxAmount = document.getElementById('tax-amount');
-//   const totalWithTax = document.getElementById('total-with-tax');
-
-//   totalCost.innerHTML = `Total Cost: $${cost.toFixed(2)}`;
-//   taxAmount.innerHTML = `Tax: $${tax.toFixed(2)}`;
-//   totalWithTax.innerHTML = `Total with Tax: $${total.toFixed(2)}`;
-// }
-
-// function showReceipt() {
-//   const receipt = document.getElementById('receipt');
-//   receipt.classList.remove('hidden');
-// }
-
-// const plantTypeSelect = document.querySelector('select[name="plant-type"]');
-// plantTypeSelect.addEventListener('change', () => {
-//   updateCost();
-// });
-
-// function placeOrder() {
-//   const selectedType = document.querySelector('select[name="plant-type"]');
-//   const quantity = parseFloat(document.getElementById('quantity').value);
-//   const plant = new Plant(selectedType, quantity);
-
-//   updateReceipt(plant);
-//   showReceipt();
-// }
-
-// const placeOrderButton = document.getElementById('place-order-btn');
-// placeOrderButton.addEventListener('click', placeOrder);
-
-// });
+// UI logic for seed-catalog
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Business Logic
- 
-
+  
   function calculateTax(cost) {
     const taxRate = 0.18;
     return cost * taxRate;
   }
 
-  // UI Logic
   function updateCost() {
-    const selectedType = document.querySelector('select[name="plant-type"]').value;
-    const quantity = parseFloat(document.getElementById('quantity').value);
-    const plant = new Plant(selectedType, quantity);
-    const cost = plant.calculateCost();
-    const costDisplay = document.getElementById('cost');
-    costDisplay.textContent = "Cost: $" + cost.toFixed(2);
+    const selectedTypes = Array.from(document.querySelectorAll('input[name="plant-type"]:checked'));
+    let totalCost = 0;
 
-    return cost;
+    selectedTypes.forEach(typeInput => {
+      const type = typeInput.value;
+      const quantityInput = document.querySelector(`input[name="quantity-${type}"]`);
+      const quantity = parseFloat(quantityInput.value);
+
+      if (quantity > 0) {
+        const plant = new Plant(type, quantity);
+        totalCost += plant.calculateCost();
+      }
+    });
+
+    const costDisplay = document.getElementById('total-cost');
+    costDisplay.textContent = "Total Cost: $" + totalCost.toFixed(2);
+
+    return totalCost;
   }
 
-  function updateReceipt(plant) {
-    const orderDetails = document.getElementById('order-details');
-    orderDetails.innerHTML = `Type: ${plant.type}<br>Quantity: ${plant.quantity}`;
+  function updateReceipt(totalCost) {
+    const totalCostDisplay = document.getElementById('total-cost');
+    const taxAmountDisplay = document.getElementById('tax-amount');
+    const totalWithTaxDisplay = document.getElementById('total-with-tax');
 
-    const cost = plant.calculateCost();
-    const tax = calculateTax(cost);
-    const total = cost + tax;
+    const tax = calculateTax(totalCost);
+    const totalWithTax = totalCost + tax;
 
-    const totalCost = document.getElementById('total-cost');
-    const taxAmount = document.getElementById('tax-amount');
-    const totalWithTax = document.getElementById('total-with-tax');
-
-    totalCost.innerHTML = `Total Cost: $${cost.toFixed(2)}`;
-    taxAmount.innerHTML = `Tax: $${tax.toFixed(2)}`;
-    totalWithTax.innerHTML = `Total with Tax: $${total.toFixed(2)}`;
+    totalCostDisplay.innerHTML = `Total Cost: $${totalCost.toFixed(2)}`;
+    taxAmountDisplay.innerHTML = `Tax: $${tax.toFixed(2)}`;
+    totalWithTaxDisplay.innerHTML = `Total with Tax: $${totalWithTax.toFixed(2)}`;
   }
 
   function showReceipt() {
@@ -112,12 +53,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const placeOrderButton = document.getElementById('place-order-btn');
   placeOrderButton.addEventListener('click', () => {
-    const selectedType = document.querySelector('select[name="plant-type"]').value;
-    const quantity = parseFloat(document.getElementById('quantity').value);
-    const plant = new Plant(selectedType, quantity);
-
-    updateReceipt(plant);
+    const totalCost = updateCost();
+    updateReceipt(totalCost);
     showReceipt();
-    updateCost();
   });
 });
