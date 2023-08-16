4f97ce4c8ac072aab1bedd64619d832d6d1c3e21
Mac-ziis <macg286@gmail.com>
2023-08-15 19:21:24 -0700

    Add the functioning form

--- src/index.js
+++ src/index.js
@@ -8,61 +8,116 @@ import Plant from './js/seed-catalog.js';
 
 
 // UI Logic for seed-catalog.js
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-function calculateTax(cost) {
-  const taxRate = 0.18;
-  return cost * taxRate;
-}
+// function calculateTax(cost) {
+//   const taxRate = 0.18;
+//   return cost * taxRate;
+// }
 
-function updateCost() {
-  const selectedType = document.querySelector('select[name="plant-type"]').value;
-  const quantity = parseFloat(document.getElementById('quantity').value);
-  const plant = new Plant(selectedType, quantity);
-  const cost = plant.calculateCost();
-  const costDisplay = document.getElementById('cost');
-  costDisplay.textContent = "Cost: $" + cost.toFixed(2);
+// function updateCost() {
+//   const selectedType = document.querySelector('select[name="plant-type"]').value;
+//   const quantity = parseFloat(document.getElementById('quantity').value);
+//   const plant = new Plant(selectedType, quantity);
+//   const cost = plant.calculateCost();
+//   const costDisplay = document.getElementById('cost');
+//   costDisplay.textContent = "Cost: $" + cost.toFixed(2);
 
-  return cost;
-}
+//   return cost;
+// }
 
-function updateReceipt(plant) {
-  const orderDetails = document.getElementById('order-details');
-  orderDetails.innerHTML = `Type: ${plant.type}<br>Quantity: ${plant.quantity}`;
+// function updateReceipt(plant) {
+//   const orderDetails = document.getElementById('order-details');
+//   orderDetails.innerHTML = `Type: ${plant.type}<br>Quantity: ${plant.quantity}`;
 
-  const cost = plant.calculateCost();
-  const tax = calculateTax(cost);
-  const total = cost + tax;
+//   const cost = plant.calculateCost();
+//   const tax = calculateTax(cost);
+//   const total = cost + tax;
 
-  const totalCost = document.getElementById('total-cost');
-  const taxAmount = document.getElementById('tax-amount');
-  const totalWithTax = document.getElementById('total-with-tax');
+//   const totalCost = document.getElementById('total-cost');
+//   const taxAmount = document.getElementById('tax-amount');
+//   const totalWithTax = document.getElementById('total-with-tax');
 
-  totalCost.innerHTML = `Total Cost: $${cost.toFixed(2)}`;
-  taxAmount.innerHTML = `Tax: $${tax.toFixed(2)}`;
-  totalWithTax.innerHTML = `Total with Tax: $${total.toFixed(2)}`;
-}
+//   totalCost.innerHTML = `Total Cost: $${cost.toFixed(2)}`;
+//   taxAmount.innerHTML = `Tax: $${tax.toFixed(2)}`;
+//   totalWithTax.innerHTML = `Total with Tax: $${total.toFixed(2)}`;
+// }
 
-function showReceipt() {
-  const receipt = document.getElementById('receipt');
-  receipt.classList.remove('hidden');
-}
+// function showReceipt() {
+//   const receipt = document.getElementById('receipt');
+//   receipt.classList.remove('hidden');
+// }
 
-const plantTypeSelect = document.querySelector('select[name="plant-type"]');
-plantTypeSelect.addEventListener('change', () => {
-  updateCost();
-});
+// const plantTypeSelect = document.querySelector('select[name="plant-type"]');
+// plantTypeSelect.addEventListener('change', () => {
+//   updateCost();
+// });
 
-function placeOrder() {
-  const selectedType = document.querySelector('select[name="plant-type"]');
-  const quantity = parseFloat(document.getElementById('quantity').value);
-  const plant = new Plant(selectedType, quantity);
+// function placeOrder() {
+//   const selectedType = document.querySelector('select[name="plant-type"]');
+//   const quantity = parseFloat(document.getElementById('quantity').value);
+//   const plant = new Plant(selectedType, quantity);
 
-  updateReceipt(plant);
-  showReceipt();
-}
+//   updateReceipt(plant);
+//   showReceipt();
+// }
 
-const placeOrderButton = document.getElementById('place-order-btn');
-placeOrderButton.addEventListener('click', placeOrder);
+// const placeOrderButton = document.getElementById('place-order-btn');
+// placeOrderButton.addEventListener('click', placeOrder);
 
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Business Logic
+ 
+
+  function calculateTax(cost) {
+    const taxRate = 0.18;
+    return cost * taxRate;
+  }
+
+  // UI Logic
+  function updateCost() {
+    const selectedType = document.querySelector('select[name="plant-type"]').value;
+    const quantity = parseFloat(document.getElementById('quantity').value);
+    const plant = new Plant(selectedType, quantity);
+    const cost = plant.calculateCost();
+    const costDisplay = document.getElementById('cost');
+    costDisplay.textContent = "Cost: $" + cost.toFixed(2);
+
+    return cost;
+  }
+
+  function updateReceipt(plant) {
+    const orderDetails = document.getElementById('order-details');
+    orderDetails.innerHTML = `Type: ${plant.type}<br>Quantity: ${plant.quantity}`;
+
+    const cost = plant.calculateCost();
+    const tax = calculateTax(cost);
+    const total = cost + tax;
+
+    const totalCost = document.getElementById('total-cost');
+    const taxAmount = document.getElementById('tax-amount');
+    const totalWithTax = document.getElementById('total-with-tax');
+
+    totalCost.innerHTML = `Total Cost: $${cost.toFixed(2)}`;
+    taxAmount.innerHTML = `Tax: $${tax.toFixed(2)}`;
+    totalWithTax.innerHTML = `Total with Tax: $${total.toFixed(2)}`;
+  }
+
+  function showReceipt() {
+    const receipt = document.getElementById('receipt');
+    receipt.classList.remove('hidden');
+  }
+
+  const placeOrderButton = document.getElementById('place-order-btn');
+  placeOrderButton.addEventListener('click', () => {
+    const selectedType = document.querySelector('select[name="plant-type"]').value;
+    const quantity = parseFloat(document.getElementById('quantity').value);
+    const plant = new Plant(selectedType, quantity);
+
+    updateReceipt(plant);
+    showReceipt();
+    updateCost();
+  });
 });
--- src/js/seed-catalog.js
+++ src/js/seed-catalog.js
@@ -12,11 +12,27 @@
         cost = 2.99;
         break;
       case "tree":
-        cost = 12.99;
+        cost = 39.99;
+        break;
+      case "fruit-tree":
+        cost = 49.99;
         break;
       case "shrub":
         cost = 6.99;
         break;
+      case "vegetable-seeds":
+        cost= 4.99;
+        break;
+      case "squash-seeds":
+        cost = 4.99;
+        break;
+      case "flower-starts":
+        cost = 12.99;
+        break;
+      case "veggie-starts":
+        cost = 14.99;
+        break;
+
     }
     return cost * this.quantity;
   };
