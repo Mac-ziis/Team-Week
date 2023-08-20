c9bd7c2f2c6ac3bfe4d655eb630eff67fd45fb76
WillJolley <yeswilljolley@gmail.com>
2023-08-20 09:13:21 -0700

    set up plant by image service and UI, weather service and UI

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import PlantService from './js/plant-finder.js';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import PlantService from "./js/plant-finder.js";
+import PlantImgService from "./js/plant-img-service.js";
+// import WeatherService from './js/weather-service.js';
 
-// Business Logic
+// Business Logic for plant-finder
 
 function getPlant(plantName) {
-  PlantService.getPlant(plantName)
-    .then(function(response) {
-      if (response.data) {
-        plantInfo(response, plantName);
-      } else {
-        printError(response);
-      }
-    });
+  PlantService.getPlant(plantName).then(function (response) {
+    if (response.data) {
+      plantInfo(response, plantName);
+    } else {
+      printError(response);
+    }
+  });
 }
 
-// UI Logic
+//Business Logic for plant-img-service
+
+function getPlantByImage(plantImage) {
+  PlantImgService.getPlantByImage(plantImage).then(function (response) {
+    if (response.results) {
+      plantImgInfo(response);
+    } else {
+      printError(response);
+    }
+  });
+}
+
+// UI Logic for plant-finder
 
 function plantInfo(response, plantName) {
   const container = document.getElementById("result-area");
@@ -35,7 +48,7 @@ function plantInfo(response, plantName) {
     Lighting Preference: ${entry.sunlight}
     </br>
     Watering Preference: ${entry.watering}`;
-  }); 
+  });
 }
 
 function printError(error) {
@@ -49,6 +62,44 @@ function handleForm(event) {
   getPlant(plantName);
 }
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleForm);
+window.addEventListener("load", function () {
+  document.getElementById("textForm").addEventListener("submit", handleForm);
 });
+
+//UI for plant-img-service
+
+function plantImgInfo(response) {
+  const container = document.getElementById("result-area");
+  container.innerHTML = `This plant might be: ${response.bestMatch}`;
+}
+
+function handleImageForm(event) {
+  event.preventDefault();
+  const plantImage = document.getElementById("image-input").value;
+  document.getElementById("image-input").value = null;
+  getPlantByImage(plantImage);
+}
+
+window.addEventListener("load", function () {
+  document.getElementById("imageForm").addEventListener("submit", handleImageForm);
+});
+
+// Ui for weather-service
+
+// function printElements(data) {
+//   document.getElementById('result-area').innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+//   The temperature in Fahrenheit is ${Math.round(1.8 *(data[0].main.temp - 273.15) + 32)} degrees.
+//   The weather in ${data[1]} is ${data[0].weather[0].description}.`;
+
+// }
+
+// function printBad(error) {
+//   document.getElementById('result-area').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+// }
+
+// function handleFormSubmission(event) {
+//   event.preventDefault();
+//   const city = document.getElementById("location").value;
+//   document.getElementById("location").value = null;
+//   getWeather(city);
+// }
---
+++ src/js/plant-img-service.js
@@ -0,0 +1,20 @@
+export default class PlantImgService {
+    static getPlantByImage() {
+      // return fetch (`https://my-api.plantnet.org/v2/identify/all?images=${plantImage}&organs=leaf&include-related-images=true&no-reject=true&lang=en&type=kt&api-key=2b10LbNjTTDY88wEIWjTGFW`)
+      return fetch (`https://my-api.plantnet.org/v2/identify/all?images=https%3A%2F%2Fi.imgur.com%2FaWM4Wap.jpg&include-related-images=false&no-reject=false&lang=en&api-key=2b105VLAeIkcZCdeWdXG0fcFF`)
+      .then(function(response) {
+        const jsonifiedResponse = response.json()
+        console.log(jsonifiedResponse)
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      });
+    }
+}
+//2b10LbNjTTDY88wEIWjTGFW
